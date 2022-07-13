74c817141498528af4c9e09b99a93bdc4b2c53c0
Mandy Mason <mnmason86@gmail.com>
2022-07-12 21:38:18 -0400

    all images from data.json rendering in React app

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,30 @@
 import { Component } from 'react';
-import HornedBeast from './HornedBeast';
+import list from '../data.json';
+import BeastList from './BeastList.js';
+// import Results from './Results.js';
+
 
 class Main extends Component {
-  
+  constructor() {
+    super();
+    this.state = {
+      currentBeast: null,
+      beastList: list,
+    }
+  }
+
+  selectCurrentBeast = (beast) => {
+    this.setState({currentBeast: beast});
+  }
+
   render(){
+    console.log(this.state.currentBeast);
     return(
-      <>
-        <HornedBeast title='Uniwhal' 
-        imageUrl='http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg' alt='Uniwhal' 
-        desc='A unicorn and a narwhal nuzzling their horns'/>
-        <HornedBeast title='Rhino Family'
-        imageUrl='https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'
-        alt='Rhino Family'
-        desc='Parent rhino with two babies'/>
-      </>
+      <div id='main'>
+        {/* <Results currentBeast={this.state.currentBeast}/> */}
+        <BeastList beasts={this.state.beastList} selectBeast={this.selectCurrentBeast} />
+      </div>
     )
   }
 }
-
 export default Main;
