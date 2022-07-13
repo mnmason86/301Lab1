62e8f8d2238621d3e832945de374ed3751fab73b
Mandy Mason <mnmason86@gmail.com>
2022-07-13 18:00:06 -0400

    added breakpoints for fluid container

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,9 @@
 import { Component } from 'react';
 import list from '../data.json';
 import HornedBeast from './HornedBeast.js';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 class Main extends Component {
   constructor() {
@@ -12,14 +15,19 @@ class Main extends Component {
 
   render(){
     return(
-      <div id='main'>
-        {this.state.beastList.map(n =>
-          <HornedBeast
-            title={n.title}
-            image_url={n.image_url}
-            description={n.description} />
-          )}
-      </div>
+      <Container fluid id='main'>
+        <Row sm={1} md={2} lg={3}>
+            {this.state.beastList.map(beast =>
+            <Col >
+              <HornedBeast
+                title={beast.title}
+                image_url={beast.image_url}
+                description={beast.description} />
+            </Col>
+              )}
+           
+          </Row>
+      </Container>
     )
   }
 }
